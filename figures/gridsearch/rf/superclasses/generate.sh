b0a7ce8f4ac00128828ce825fe5e6201c88739e4
Markus Beuckelmann <email@markus-beuckelmann.de>
2015-06-22 15:07:20 +0200

    Add figures for RF superclass grid search

---
+++ figures/gridsearch/rf/superclasses/generate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./gridsearch-heatmap.py --grid 2015-06-16-00:41:07.605388-grid.pkl --midpoint 0.975 --save rf-superclasses-01.png
+./gridsearch-heatmap.py --grid 2015-06-16-11:26:43.936451-grid.pkl --save rf-superclasses-02.png
+./gridsearch-heatmap.py --grid 2015-06-17-00:15:08.050355-grid.pkl --save rf-superclasses-03.png
+./gridsearch-heatmap.py --grid 2015-06-17-16:49:17.264005-grid.pkl --save rf-superclasses-04.png
