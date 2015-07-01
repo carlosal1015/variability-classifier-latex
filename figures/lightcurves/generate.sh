3a204856866c29fcd23b44cc553baaad10e676f7
Markus Beuckelmann <email@markus-beuckelmann.de>
2015-07-01 21:45:42 +0200

    Add light curve figures

---
+++ figures/lightcurves/generate.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+./plot-lightcurve.py --lightcurve lm0010k23412 --name ceph
+./plot-lightcurve.py --lightcurve lm0044m18106 --name t2ceph
+./plot-lightcurve.py --lightcurve lm0010l20658 --name eb
+./plot-lightcurve.py --lightcurve lm0010m4166 --name rrl
+./plot-lightcurve.py --lightcurve lm0036m6817 --name dsct
+./plot-lightcurve.py --lightcurve lm0107l17423 --band r --name lpv
+./plot-lightcurve.py --lightcurve lm0585n7181 --band r --name qso
+./plot-lightcurve.py --lightcurve lm0581k5892 --band r --name bv
+
+#./plot-lightcurve.py --lightcurve lm0010l11256 --name ceph2
+#./plot-lightcurve.py --lightcurve lm0010l28036 --name ceph3
+#./plot-lightcurve.py --lightcurve lm0093l11103 --name t2ceph2
+#./plot-lightcurve.py --lightcurve lm0010n29148 --name eb2
+#./plot-lightcurve.py --lightcurve lm0011m21666 --name eb3
+#./plot-lightcurve.py --lightcurve lm0010m16383 --name rrl2
+#./plot-lightcurve.py --lightcurve lm0010n10612 --name rrl3
+#./plot-lightcurve.py --lightcurve lm0010n20239 --name rrl4
+#./plot-lightcurve.py --lightcurve lm0040l14392 --name dsct2
+#./plot-lightcurve.py --lightcurve lm0010n29505 --name dsct3
+#./plot-lightcurve.py --lightcurve lm0010n30010 --name lpv2
+#./plot-lightcurve.py --lightcurve lm0551k28915 --band r --name qso2
+#./plot-lightcurve.py --lightcurve lm0052n17132 --name qso3
+#./plot-lightcurve.py --lightcurve lm0054n7014 --name qso4
+#./plot-lightcurve.py --lightcurve lm0124n22365 --name qso5
+#./plot-lightcurve.py --lightcurve lm0016m7543 --name nonvar2
+#./plot-lightcurve.py --lightcurve lm0020m21042 --name nonvar3
+#./plot-lightcurve.py --lightcurve lm0021l16192 --name bv2
+#./plot-lightcurve.py --lightcurve lm0021l24944 --name bv3
+#./plot-lightcurve.py --lightcurve lm0024k25600 --name bv4
